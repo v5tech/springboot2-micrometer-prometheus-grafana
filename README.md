ba0c856b9bbb281eb0bd42677841eaa04184070a
冯靖 <sxyx2008@163.com>
2020-04-24 15:20:20 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# springboot2-micrometer-prometheus-grafana
+
+# 参考文档
+
+https://prometheus.io/docs/prometheus/latest/installation
+
+https://prometheus.io/docs/prometheus/latest/getting_started
+
+https://prometheus.io/docs/guides/cadvisor
+
+https://micrometer.io/docs/registry/prometheus
+
+https://blog.csdn.net/aixiaoyang168/article/details/100866159
+
+https://www.jianshu.com/p/1cb66d48920b
+
+https://www.jianshu.com/p/1f05476ebcee
+
+https://github.com/percona/grafana-dashboards
+
+https://github.com/oliver006/redis_exporter
+
+# Grafana Dashboard
+
+https://grafana.com/grafana/dashboards/7362
+
+https://grafana.com/grafana/dashboards/4031
+
+https://grafana.com/grafana/dashboards/763
+
+https://grafana.com/grafana/dashboards/4701
+
+https://grafana.com/grafana/dashboards/8563
+
+https://grafana.com/grafana/dashboards/3457
+
+https://grafana.com/grafana/dashboards/10619
---
+++ src/main/java/net/ameizi/Springboot2MicrometerPrometheusGrafanaApplication.java
@@ -0,0 +1,23 @@
+package net.ameizi;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Springboot2MicrometerPrometheusGrafanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2MicrometerPrometheusGrafanaApplication.class, args);
+    }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(
+            @Value("${spring.application.name}") String applicationName) {
+        return (registry) -> registry.config().commonTags("application", applicationName);
+    }
+
+}
