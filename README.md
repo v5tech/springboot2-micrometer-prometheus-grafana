4581a1939451a1dae054c4d61209b10797e9b585
冯靖 <sxyx2008@163.com>
2020-04-26 11:38:51 +0800

    add Screenshots

--- README.md
+++ README.md
@@ -1,5 +1,30 @@
 # springboot2-micrometer-prometheus-grafana
 
+# Screenshots
+
+![](Screenshots/prometheus_alerts.png)
+![](Screenshots/prometheus_config.png)
+![](Screenshots/prometheus_disco.png)
+![](Screenshots/prometheus_rules.png)
+![](Screenshots/prometheus_targets.png)
+![](Screenshots/prometheus_alertmanager.png)
+![](Screenshots/cadvisor.png)
+![](Screenshots/grafana_jvm.png)
+![](Screenshots/grafana_redis_dashboard_for_prometheus.png)
+![](Screenshots/grafana_docker_container_hostmetrics.png)
+![](Screenshots/skywalking_1.png)
+![](Screenshots/skywalking_2.png)
+![](Screenshots/skywalking_3.png)
+![](Screenshots/skywalking_4.png)
+
+skywalking 添加jvm启动参数
+
+```
+-javaagent:/Users/amz/develop/skywalking-apm/agent/skywalking-agent.jar
+-DSW_AGENT_COLLECTOR_BACKEND_SERVICES=127.0.0.1:11800
+-DSW_AGENT_NAME=springboot2-micrometer-prometheus-grafana
+```
+
 # 参考文档
 
 https://prometheus.io/docs/prometheus/latest/installation
@@ -24,13 +49,9 @@ https://github.com/oliver006/redis_exporter
 
 https://grafana.com/grafana/dashboards/7362
 
-https://grafana.com/grafana/dashboards/4031
-
 https://grafana.com/grafana/dashboards/763
 
 https://grafana.com/grafana/dashboards/4701
-
-https://grafana.com/grafana/dashboards/8563
 
 https://grafana.com/grafana/dashboards/3457
 
--- src/main/java/net/ameizi/Springboot2MicrometerPrometheusGrafanaApplication.java
+++ src/main/java/net/ameizi/Springboot2MicrometerPrometheusGrafanaApplication.java
@@ -1,14 +1,30 @@
 package net.ameizi;
 
 import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+@RestController
 @SpringBootApplication
 public class Springboot2MicrometerPrometheusGrafanaApplication {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(Springboot2MicrometerPrometheusGrafanaApplication.class, args);
@@ -20,4 +36,19 @@ public class Springboot2MicrometerPrometheusGrafanaApplication {
         return (registry) -> registry.config().commonTags("application", applicationName);
     }
 
+    @GetMapping("/hello")
+    public String hello(@RequestParam("name")String name){
+        String str = restTemplate.getForObject(
+                "http://localhost:8088/name?name={name}",
+                String.class,
+                name
+        );
+        str = StringUtils.isEmpty(str)?"skywalking":name;
+        return String.format("hello,%s!", str);
+    }
+
+    @GetMapping("/name")
+    public String name(@RequestParam("name") String name){
+        return name;
+    }
 }
